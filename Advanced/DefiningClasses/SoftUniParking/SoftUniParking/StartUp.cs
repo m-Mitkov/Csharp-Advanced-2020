298ff4a931bcb714444418d88f2d0b91946e7eb0
Martin Mitkov <m.mitkov07@gmail.com>
2020-06-11 21:44:58 +0200

    11/06/2020

---
+++ Advanced/DefiningClasses/SoftUniParking/SoftUniParking/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SoftUniParking
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //var car = new Car("Skoda", "Fabia", 65, "CC1856BG");
+            //var car2 = new Car("Audi", "A3", 110, "EB8787MN");
+            //var car3 = new Car("Audi", "A3", 110, "EB8787dMN");
+            //var car4 = new Car("Audi", "A3", 110, "EB8787sMN");
+            //var car5 = new Car("Audi", "A3", 110, "EB8787gMN");
+            //var car6 = new Car("Audi", "A3", 110, "EB8787tMN");
+            
+            //Console.WriteLine(car);
+
+            //var parking = new Parking(5);
+
+            //Console.WriteLine(parking.AddCar(car));
+            //Console.WriteLine(parking.AddCar(car));
+            //Console.WriteLine(parking.AddCar(car2));
+            //Console.WriteLine(parking.AddCar(car3));
+            //Console.WriteLine(parking.AddCar(car4));
+            //Console.WriteLine(parking.AddCar(car5));
+            //Console.WriteLine(parking.AddCar(car6));
+
+            //Console.WriteLine(parking.GetCar("EB8787MN"));
+
+            //Console.WriteLine(parking.RemoveCar("EB8787MN"));
+            //Console.WriteLine(parking.RemoveCar("213"));
+
+            //Console.WriteLine(parking.Count);
+        }
+    }
+}
