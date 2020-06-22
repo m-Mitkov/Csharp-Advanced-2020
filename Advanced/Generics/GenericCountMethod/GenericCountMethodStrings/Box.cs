457c643d85016ae0fd89a5ebb048fd7cb07553d6
Martin Mitkov <m.mitkov07@gmail.com>
2020-06-22 14:41:07 +0200

    22/06/2020

---
+++ Advanced/Generics/GenericCountMethod/GenericCountMethodStrings/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethod
+{
+    class Box<T> where T : IComparable<T>
+    {
+        private List<T> values;
+        public Box()
+        {
+            this.values = new List<T>();
+        }
+        public T Value { get; set; }
+
+        public void Add(T value)
+        {
+            this.values.Add(value);
+        }
+
+        public int FilterTGreatherThanParameter(T parameter)
+        {
+            int countGreatherElements = 0;
+
+            foreach (T item in this.values)
+            {
+                if (item.CompareTo(parameter) > 0)
+                {
+                    countGreatherElements++;
+                }
+            }
+
+            return countGreatherElements;
+        }
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {this.Value}";
+        }
+    }
+}
---
+++ Advanced/Generics/GenericCountMethod/GenericCountMethodStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfElements = int.Parse(Console.ReadLine());
+            //Box<string> data = new Box<string>();
+            //
+            //for (int i = 0; i < numberOfElements; i++)
+            //{
+            //    string element = Console.ReadLine();
+            //
+            //    data.Add(element);
+            //}
+            //
+            //string elementToCompareWith = Console.ReadLine();
+            //
+            //Console.WriteLine(data.FilterTGreatherThanParameter(elementToCompareWith));
+
+           Box<double> data = new Box<double>();
+           
+           for (int i = 0; i < numberOfElements; i++)
+           {
+               data.Add(double.Parse(Console.ReadLine()));
+           }
+           
+           double parameter = double.Parse(Console.ReadLine());
+           Console.WriteLine(data.FilterTGreatherThanParameter(parameter));
+        }             
+    }
+}
