5c935692739a3a49e120b0f3379bbf97e1f26d35
Martin Mitkov <m.mitkov07@gmail.com>
2020-07-12 09:27:59 +0200

    12/07/2020

---
+++ OOP/Encapsulation/AnimalFarm/AnimalFarm/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AnimalFarm
+{
+    using System;
+    using AnimalFarm.Models;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine(
+                    "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                    chicken.Name,
+                    chicken.Age,
+                    chicken.ProductPerDay);
+
+            }
+            catch (global::System.Exception ex)
+            {
+
+                global::System.Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ OOP/Encapsulation/shoppingSpree/shoppingSpree/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace shoppingSpree
+{
+    class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bagProducts;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bagProducts = new List<Product>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be an empty");
+                }
+
+                this.name = value;
+            }
+        }
+        public decimal Money
+        {
+            get => this.money;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        public string BuyProduct(Product product)
+        {
+            if (this.money - product.Cost >= 0)
+            {
+                this.bagProducts.Add(product);
+                this.money -= product.Cost;
+
+                return $"{this.Name} bought {product.Name}";
+            }
+
+            return $"{this.Name} can't afford {product.Name}";
+        }
+
+        public override string ToString()
+        {
+            if (bagProducts.Count != 0)
+            {
+                return $"{this.name} - {string.Join(", ", this.bagProducts.Select(x => x.Name))}";
+            }
+
+            return $"{this.name} - Nothing bought";
+        }
+    }
+}
